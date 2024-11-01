f646bd7a078218f3ea6385c0c6fbd48f230396b1
Karina Koliada <koladakarina1112@gmail.com>
2024-11-01 15:47:15 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-// write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+const wallRect = wall.getBoundingClientRect();
+const wallCenterX = wallRect.width / 2;
+const wallCenterY = wallRect.height / 2;
+
+spider.style.position = 'absolute';
+spider.style.left = `${wallCenterX - spider.offsetWidth / 2}px`;
+spider.style.top = `${wallCenterY - spider.offsetHeight / 2}px`;
