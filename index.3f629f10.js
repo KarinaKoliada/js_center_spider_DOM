953ea5b61ff8f099830b20f49a002657a42aebfa
Karina Koliada <koladakarina1112@gmail.com>
2024-11-01 15:52:40 +0100

    make build

---
+++ index.3f629f10.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=e.clientWidth,l=e.clientHeight;t.style.top="".concat((c-t.height)/2,"px"),t.style.left="".concat((l-t.width)/2,"px");
+//# sourceMappingURL=index.3f629f10.js.map
